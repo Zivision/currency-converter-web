6c7e14fb6bd6c2f4d1ec45df8052b1bc790ce3c7
Zachary <zcastro703@gmail.com>
2023-07-13 08:07:30 -0500

    Added Project to Repo

---
+++ app/main.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+from flask import Flask, render_template, request
+
+import json
+
+app = Flask(__name__)
+
+# Temp value
+currencies = [
+    {"code": "GBP", "amount": 0.770},
+    {"code": "EUR", "amount": 1.11321},
+    {"code": "USD", "amount": 1.000}
+]
+
+# This function takes the database dataset and challenges it with the input data.
+# After that, it returns a dictonary with all the needed parameters.
+def match_currency_type(dataset: list[dict], input_data: dict) -> dict or None:
+    for x in dataset:
+        for y in dataset:
+            if (x["code"] == input_data["leftType"] and
+                    y["code"] == input_data["rightType"]):
+                return dict(
+                    amount_of_right=input_data["inputAmount"],
+                    left_type=x["code"],
+                    right_type=y["code"],
+                    left_in_usd=x["amount"],
+                    right_in_usd=y["amount"]
+                )
+    return None
+
+# Index route
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html", data=currencies)
+
+# Data Process Index
+@app.route("/process/convert", methods=["POST"])
+def process():
+    try:
+        json_data = request.json
+        processed_data = match_currency_type(currencies, json_data)
+        return {"result": (processed_data["amount_of_right"]/processed_data["right_in_usd"]) * processed_data["left_in_usd"]}
+    except Exception as error:
+        return "Incorrect Content-type \nError: {}".format(error)
+    
+
---
+++ app/static/js/main.js
@@ -0,0 +1,25 @@
+// Simple function for getting input from the input(inputAmount) when button(convert) is pressed
+const getInput = () => {
+    let inputAmount = Number(document.getElementById("inputAmount").value);
+    let left = document.getElementById("left").value;
+    let right = document.getElementById("right").value;
+
+    // Sends post back to the server with the two currency types
+    // along with how much of the right operators currency there is
+    fetch("http://localhost:5000/process/convert", {
+	method: "POST",
+	body: JSON.stringify({
+	    "inputAmount": inputAmount,
+	    "leftType": left,
+	    "rightType": right
+	}),
+	headers: {
+	    "Content-type": "application/json"
+	}
+    })
+	.then((response) => response.json())
+	.then((json) => {
+	    document.getElementById("result").innerHTML = `Result: ${Number(json.result).toFixed(2)}`
+	});
+}
+
